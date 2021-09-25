6d318a134c46f44f18d0ac9e324f1b8caf11b774
Fabian Martinez <fabian.m.martinez@gmail.com>
2021-09-25 11:04:43 -0700

    - Add methods to save text - Add conditions to determine if edit or save

--- src/App.js
+++ src/App.js
@@ -1,14 +1,25 @@
+import React from 'react';
 import './App.css';
 import {Button, TextField, Typography} from '@mui/material';
 import EditIcon from '@mui/icons-material/Edit';
 
 function App() {
+    const [ canEdit, setCanEdit ] = React.useState(false);
+
+    const editField = React.useCallback(() => {
+        setCanEdit(true);
+    }, [canEdit]);
+
+    const saveField = React.useCallback(() => {
+        setCanEdit(false);
+    }, [canEdit]);
+
   return (
     <div className="App">
         <div>
-            <TextField disabled id="standard-basic" label="Standard" variant="standard" defaultValue="Hello World"/>
+            <TextField disabled={!canEdit} id="standard-basic" label="Standard" variant="standard" defaultValue="Hello World"/>
 
-            <Button variant="outlined" startIcon={<EditIcon />}>Edit</Button>
+            <Button variant="outlined" startIcon={<EditIcon />} onClick={() => {!canEdit ? editField() : saveField()}}>{canEdit ? 'Save' : 'Edit'}</Button>
         </div>
     </div>
   );
